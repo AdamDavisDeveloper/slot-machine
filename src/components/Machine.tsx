081aa29a86fcb76519c14865c906a6388d048ef4
AdamDavisDeveloper <adavisdeveloper@gmail.com>
2024-03-09 15:10:22 -0500

    more prelim work on slot component

--- src/components/Machine.tsx
+++ src/components/Machine.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
 import Slot from "./Slot";
 import '/src/styles/machine.scss'
 
 export default function Machine(props: any) {
+    const [ machineIsActive, setMachineIsActive ]   = useState<boolean>(false);
+    const [ activeSlotID, setActiveSlotID ]         = useState<number>(0);
+    const [ wager, setWager ]                       = useState<number>(0);
+
+
+    useEffect(() => {
+        if(wager === 1) setMachineIsActive(true);
+    }, [wager]);
+
+    useEffect(() => {
+        console.log(machineIsActive);
+    }, [machineIsActive]);
+
     return (
         <div id="Machine">
-            <Slot />
-            <Slot />
-            <Slot />
+            <span>{`${wager}`}</span>
+            <div id="Slots">
+                <Slot machineIsActive={machineIsActive} />
+                <Slot machineIsActive={machineIsActive} />
+                <Slot machineIsActive={machineIsActive} />
+            </div>
+            {
+                wager <3 && 
+                <button onClick={() => setWager((wager) => wager + 1)}>Wager</button>
+            }
+            {/* <button onClick={() => }>Stop</button> */}
         </div>
     );
 };
--- src/components/Slot.tsx
+++ src/components/Slot.tsx
@@ -1,20 +1,42 @@
 import { useEffect, useState } from 'react';
+import ViteLogo from '/public/vite.svg';
 import '/src/styles/slot.scss'
 
-export default function Slot(props: any) {
-    const SlotItem = (contents: string) => {
+export default function Slot(props: {
+    machineIsActive: boolean,
+}) {
+    const { machineIsActive } = props;
+    const [ slotIsActive, setSlotIsActive ] = useState<boolean>(false);
+
+    // Effect Hooks
+    useEffect(() => {
+        setSlotIsActive(!!machineIsActive);
+    }, [machineIsActive]);
+
+    useEffect(() => {
+        console.log("Item: ", items[0]);
+    }, []);
+
+
+    const slotItems = [
+        { id: 1, imgPath: ViteLogo },
+        { id: 2, imgPath: ViteLogo },
+        { id: 3, imgPath: ViteLogo },
+    ];
+    const items: any[] = [ 
+        SlotItem(slotItems[0]),
+        SlotItem(slotItems[1]),
+        SlotItem(slotItems[2]),
+    ];
+
+    function SlotItem(contents: {id: number, imgPath: string}) {
         return (
             <div className="slot-item">
-                <p>{contents}</p>
+                <p>{contents.id}</p>
             </div>
         )
     };
 
-    const [ slotIsActive, setSlotIsActive ] = useState(true);
-
-    const items: any[] = [ 
-        SlotItem("1"),
-    ];
 
 
     function slotWheelEngine() {
@@ -24,10 +46,6 @@ export default function Slot(props: any) {
             }
         }
     }
-
-    useEffect(() => {
-        console.log("Item: ", items[0]);
-    }, []);
 
     //function stopSlotWheel() { setSlotIsActive(false); }
 
