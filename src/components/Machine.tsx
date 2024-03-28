6d50a31e7a2a528d2eb8afcdc47249ae8663b5d5
AdamDavisDeveloper <adavisdeveloper@gmail.com>
2024-03-28 17:28:16 -0400

    tracking item IDs in currentIndex + start calculating win conditions

--- src/components/Machine.tsx
+++ src/components/Machine.tsx
@@ -3,45 +3,101 @@ import Slot from "./Slot";
 import Crossbars from "./Crossbars";
 import '/src/styles/machine.scss'
 
+export interface slotItemPosition { id: number, positions: {id: number, currentIndex: number}[] };
+
 export default function Machine(props: {
     adjustUserCoins: (adjustment: number) => void,
 }) {
     const [ machineIsActive, setMachineIsActive ]   = useState<boolean>(false);
-    const [ activeSlotID, setActiveSlotID ]         = useState<number>(0);
+    const [ activeSlotID, setActiveSlotID ]         = useState<number>(0); // Used to determine which slot to stop + when all slots have been stopped
     const [ wager, setWager ]                       = useState<number>(0);
+    const [ childStates, setChildStates ]           = useState<any>({});
 
+    
     const { adjustUserCoins } = props;
-
+    
     useEffect(() => {
         if(wager === 1) setMachineIsActive(true);
         console.log(wager, machineIsActive);
     }, [wager]);
 
     useEffect(() => {
-        if(activeSlotID === 3) resetMachine();
-    }, [activeSlotID]);
+        console.log(childStates)
+        if(childStates["3"]) calculateWinnings();
+    }, [childStates]);
 
+    function doIndexesMatch(first: number, second: number, third: number): boolean {
+        return (first === second && second === third) ? true : false;
+    }
+
+    function calculateWinnings() {
+        const slotOne = childStates["1"], slotTwo = childStates["2"], slotThree = childStates["3"];
+
+        if (!slotOne || !slotTwo || !slotThree) {
+            console.error("One or more slots have not submitted their positions.");
+            return;
+        }
+        console.log("Calculating winnings....");
+        console.log(slotOne, slotTwo, slotThree)
+        const machineRows = {
+            center:         doIndexesMatch(slotOne.positions[1].id, slotTwo.positions[1].id, slotThree.positions[1].id),
+            top:            doIndexesMatch(slotOne.positions[0].id, slotTwo.positions[0].id, slotThree.positions[0].id),
+            bottom:         doIndexesMatch(slotOne.positions[2].id, slotTwo.positions[2].id, slotThree.positions[2].id),
+            diagForward:    doIndexesMatch(slotOne.positions[2].id, slotTwo.positions[1].id, slotThree.positions[0].id),
+            diagBackward:   doIndexesMatch(slotOne.positions[0].id, slotTwo.positions[1].id, slotThree.positions[2].id),
+        };
+        console.log("machineRows:", machineRows);
+        console.log("diagBack row:", slotOne.positions[0].id, slotTwo.positions[1].id, slotThree.positions[2].id);
+        if(wager === 1) {
+            if(machineRows.center) {
+                console.log("Center win", slotOne.positions[1].id);
+            }
+            else console.log("No win :(");
+            
+        }
+        if(wager === 2) {
+            if(machineRows.center)  console.log("Center win", slotOne.positions[1].id);
+            if(machineRows.top)     console.log("Top win", slotOne.positions[0].id);
+            if(machineRows.bottom)  console.log("Bottom win", slotOne.positions[2].id);
+            else console.log("No win :(");
+        }
+        if(wager === 3) {
+            if(machineRows.center)          console.log("Center win", slotOne.positions[1].id);
+            if(machineRows.top)             console.log("Top win", slotOne.positions[0].id);
+            if(machineRows.bottom)          console.log("Bottom win", slotOne.positions[2].id);
+            if(machineRows.diagForward)     console.log("DiagFW win", slotOne.positions[2].id);
+            if(machineRows.diagBackward)    console.log("DiagBW win", slotOne.positions[2].id);
+            else console.log("No win :(");
+        }
+        //resetMachine();
+    };
+    
     function resetMachine(): void {
         setActiveSlotID(0);
         setWager(0);
         setMachineIsActive(false);
-        adjustUserCoins(20); //TODO: this needs to be actual prize amounts.
+        setChildStates({});
+        adjustUserCoins(20); //TODO: this needs to be actual prize amounts and moved elsewhere.
     }
-
+    
     function wagerMaxed(): boolean {
         return !!(wager >= 3);
     }
-
+    
     function stopSlot() { setActiveSlotID((id) => id + 1) };
+
+    const receiveChildState = (slotPositions: slotItemPosition) => {
+        setChildStates((prevState: any) => ({ ...prevState, [slotPositions.id]: slotPositions }));
+    };
 
     return (
         <>
             <div id="Machine">
                 <span>{`${wager}`}</span>
                 <div id="Slots">
-                    <Slot id={1} machineIsActive={machineIsActive} activeSlotID={activeSlotID} />
-                    <Slot id={2} machineIsActive={machineIsActive} activeSlotID={activeSlotID} />
-                    <Slot id={3} machineIsActive={machineIsActive} activeSlotID={activeSlotID} />
+                    <Slot id={1} machineIsActive={machineIsActive} activeSlotID={activeSlotID} submitSlotPositions={receiveChildState} />
+                    <Slot id={2} machineIsActive={machineIsActive} activeSlotID={activeSlotID} submitSlotPositions={receiveChildState} />
+                    <Slot id={3} machineIsActive={machineIsActive} activeSlotID={activeSlotID} submitSlotPositions={receiveChildState} />
                 </div>
 
                 <button className={`${wagerMaxed() ? 'disabled' : ''}`} onClick={() => setWager((wager) => wager + 1)}>{wagerMaxed() ? 'Max' : 'Wager'}</button>
--- src/components/Slot.tsx
+++ src/components/Slot.tsx
@@ -5,82 +5,103 @@ import ReactLogo from '/react.svg';
 import NodeLogo from '/nodejs.svg';
 import RubyLogo from '/ruby.svg';
 import HaskellLogo from '/haskell.svg';
-//import RustLogo from '/rust.svg';
+import RustLogo from '/rust.svg';
+import FirebaseLogo from '/firebase.svg';
+import SpinAgain from '/spinagain.svg';
 import '/src/styles/slot.scss'
+
+import { slotItemPosition } from './Machine';
 
 export default function Slot(props: {
     id: number, // Describes which slot wheel
-    machineIsActive: boolean,
+    machineIsActive: boolean, // Set true when user wagers 1 or more coin
     activeSlotID: number,
+    submitSlotPositions: (data: slotItemPosition) => void,
 }) {
-    const { id, machineIsActive, activeSlotID } = props;
+    const { id, machineIsActive, activeSlotID, submitSlotPositions } = props;
     const [ slotIsActive, setSlotIsActive ] = useState<boolean>(false);
-    const [ currentIndex, setCurrentIndex ] = useState<number>(1);
+    const [ currentIndex, setCurrentIndex ] = useState<currentIndex>({id: 1, currentIndex: 0});
     const animationSpeed: number = 40 * id; // Each subsequent slot spins slower than the previous.
 
     const itemDefinitions: SlotItem[] = [
-        { id: 1, imgPath: ViteLogo  },
-        { id: 2, imgPath: HTMLLogo  },
-        { id: 3, imgPath: ReactLogo },
-        { id: 4, imgPath: NodeLogo  },
-        { id: 5, imgPath: RubyLogo  },
-        { id: 6, imgPath: HaskellLogo  },
+        //id: reference to item type/value -- imgPath: Vite import of image (top of file)
+        { id: 1, imgPath: ViteLogo      },
+        { id: 2, imgPath: HTMLLogo      },
+        { id: 3, imgPath: ReactLogo     },
+        { id: 4, imgPath: NodeLogo      },
+        { id: 5, imgPath: RubyLogo      },
+        { id: 6, imgPath: HaskellLogo   },
+        { id: 7, imgPath: RustLogo      },
+        { id: 8, imgPath: FirebaseLogo  },
+        { id: 9, imgPath: SpinAgain     },
     ];
 
     const slotItems: ReactElement[] = itemDefinitions.map(def => CreateSlotItem(def));
 
-    // Interfaces
-    interface SlotItem { id: number, imgPath: string };
+    // ------------- Interfaces -------------
+    interface SlotItem      { id: number, imgPath: string };
+    interface currentIndex  { id: number, currentIndex: number };
 
-    // Effect Hooks
+    // ------------- Effect Hooks -------------
     useEffect(() => {
         setSlotIsActive(!!machineIsActive);
     }, [machineIsActive]);
 
+
     useEffect(() => {
-        // Sets false if the stop button has been pressed enough for activeSlotID to reach this slot's ID
         if(activeSlotID === 0) return;
+        if(activeSlotID >= id) {
+            const wrappedIndex = (index: number) => (index + itemDefinitions.length) % itemDefinitions.length;
+            submitSlotPositions({
+                id: id, 
+                positions: [
+                    { id: itemDefinitions[wrappedIndex(currentIndex.currentIndex - 1)].id, currentIndex: currentIndex.currentIndex - 1}, 
+                    { id: itemDefinitions[wrappedIndex(currentIndex.currentIndex)].id, currentIndex: currentIndex.currentIndex},
+                    { id: itemDefinitions[wrappedIndex(currentIndex.currentIndex + 1)].id, currentIndex: currentIndex.currentIndex + 1},
+                ]
+            });
+        }
         setSlotIsActive(!(activeSlotID >= id));
-    }, [activeSlotID]);
+    }, [activeSlotID, currentIndex]);
+    
 
     useEffect(() => {
         if(!slotIsActive) return;
         const timer = setTimeout(() => {
-            setCurrentIndex((curr) => (curr + 1) % slotItems.length);
+            setCurrentIndex((prevState: currentIndex) => ({
+                id: itemDefinitions[(prevState.currentIndex + 1) % slotItems.length].id,
+                currentIndex: (prevState.currentIndex + 1) % slotItems.length
+            }));
+            //setCurrentIndex((curr) => (curr + 1) % slotItems.length);
         }, animationSpeed);
         return () => clearTimeout(timer);
     }, [currentIndex, slotIsActive, slotItems.length]);
     
 
+    // ------------- Utility Functions -------------
     function getWrappedIndex(index: number, length: number): number {
-        if (index < 0) {
-            return length - 1;
-        }
-        if (index >= length) {
-            return 0;
-        }
+        if (index < 0) return length - 1;
+        if (index >= length) return 0;
         return index;
     };    
 
 
     function CreateSlotItem(definition: SlotItem): ReactElement {
-        return <img src={definition.imgPath} alt="slot item icon" />;
+        return <img key={definition.id} src={definition.imgPath} alt="slot item icon" />;
     };
-
-    //function stopSlotWheel() { setSlotIsActive(false); }
 
     return (
         <>
             <div className="slot-window">
                 <div className="slot">
                     <div className="slot-item">
-                        { slotItems[getWrappedIndex(currentIndex - 1, slotItems.length)] }
+                        { slotItems[getWrappedIndex(currentIndex.currentIndex - 1, slotItems.length)] }
                     </div>
                     <div className="slot-item">
-                        { slotItems[currentIndex] }
+                        { slotItems[currentIndex.currentIndex] }
                     </div>
                     <div className="slot-item">
-                        { slotItems[getWrappedIndex(currentIndex + 1, slotItems.length)] }
+                        { slotItems[getWrappedIndex(currentIndex.currentIndex + 1, slotItems.length)] }
                     </div>
                 </div>
             </div>
